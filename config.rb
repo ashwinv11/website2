33702b7b8a080c5f80ce72624a74fcca2ae38ff5
ashwinv11 <ashwinv11@gmail.com>
2017-10-13 17:48:15 -0700

    fixing broken blog paths

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 ###
 
 require 'tzinfo'
-Time.zone = "America/Los_Angeles"
+Time.zone = 'America/Los_Angeles'
 Haml::TempleEngine.disable_option_validator!
 
 # Per-page layout changes:
@@ -13,17 +13,16 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-activate :directory_indexes
 set :haml, { ugly: true, format: :html5 }
 
-activate :i18n, templates_dir: "views"
+activate :i18n, templates_dir: 'views'
 set :layouts_dir, 'views/layouts'
 
 # Webpack asset pipeline
 activate :external_pipeline,
          name: :webpack,
-         command: build? ? "./node_modules/webpack/bin/webpack.js --bail -p" : "./node_modules/webpack/bin/webpack.js --watch -d --progress --color",
-         source: ".tmp/dist",
+         command: build? ? './node_modules/webpack/bin/webpack.js --bail -p' : './node_modules/webpack/bin/webpack.js --watch -d --progress --color',
+         source: '.tmp/dist',
          latency: 1
 
 set :css_dir, 'assets/stylesheets'
@@ -32,14 +31,14 @@ set :images_dir, 'assets/images'
 
 
 # With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+# page '/path/to/file.html', layout: :otherlayout
 
 # With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+# page '/path/to/file.html', layout: :otherlayout
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+# proxy '/this-page-has-no-template.html', '/template-file.html', locals: {
+#  which_fake_page: 'Rendering a fake page with a local variable' }
 
 # General configuration
 
@@ -49,15 +48,16 @@ set :images_dir, 'assets/images'
 # end
 
 activate :blog do |blog|
-  blog.sources = "views/posts/:title.html"
-  blog.prefix = "projects"
-  blog.permalink = "{title}.html"
+  blog.sources = 'projects/:title.html'
+  blog.permalink = 'projects/{title}.html'
 
-  blog.default_extension = ".haml"
+  blog.default_extension = '.haml'
   # blog.paginate = true
   # blog.per_page = 10
-  # blog.page_link = "page/{num}"
+  # blog.page_link = 'page/{num}'
 end
+
+activate :directory_indexes
 
 ###
 # Helpers
@@ -66,7 +66,7 @@ end
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
-#     "Helping"
+#     'Helping'
 #   end
 # end
 
