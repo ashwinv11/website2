c9c6918c5a54df57bcf003d3c429146a6a22521b
ashwinv11 <ashwinv11@gmail.com>
2017-10-26 21:09:40 -0700

    rename all.js to ga.js

--- source/assets/javascripts/contact.js
+++ source/assets/javascripts/contact.js
@@ -1,13 +1,13 @@
-var a = document.createElement("a");
-a.setAttribute('href', "mailto:ashwin@vaswani.us?Subject=Hello%20Ashwin");
+var a = document.createElement('a');
+a.setAttribute('href', 'mailto:ashwin@vaswani.us?Subject=Hello%20Ashwin');
 a.setAttribute('target', '_top');
 
-var h1 = document.createElement("h1");
-var node = document.createTextNode("ashwin [at] vaswani.us");
+var h1 = document.createElement('h1');
+var node = document.createTextNode('ashwin [at] vaswani.us');
 h1.appendChild(node);
 a.appendChild(h1);
 
 function showAddress(){
-  var check = document.getElementById("checker");
+  var check = document.getElementById('checker');
   check.parentNode.replaceChild(a, check);
 }
