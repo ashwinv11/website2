6f7613e35ac63226c87a3958d8f1f553830e5102
ashwinv11 <ashwinv11@gmail.com>
2017-11-04 23:25:53 -0700

    change vars to consts and lets

--- source/assets/javascripts/bezier.js
+++ source/assets/javascripts/bezier.js
@@ -1,9 +1,9 @@
-var t;
-var frameCount;
-var colorSwitch = false;
+let colorSwitch = false;
+let frameCount = 0;
+let t = 0;
 
 function setup() {
-  var myCanvas = createCanvas(380, 380);
+  const myCanvas = createCanvas(380, 380);
   myCanvas.parent('p5');
   myCanvas.mousePressed(resetDraw);
   myCanvas.touchStarted(resetDraw);
@@ -11,8 +11,6 @@ function setup() {
   background(242, 242, 242);
   stroke(209, 55, 55, 25);
   noFill();
-  t = 0;
-  frameCount = 0;
 }
 
 function resetDraw(){
@@ -31,19 +29,19 @@ function draw() {
     stroke(209, 55, 55, 25);
   }
 
-  var x1 = width * noise(t + 15);
-  var x2 = width * noise(t + 25);
-  var x3 = width * noise(t + 35);
-  var x4 = width * noise(t + 45);
-  var y1 = height * noise(t + 55);
-  var y2 = height * noise(t + 65);
-  var y3 = height * noise(t + 75);
-  var y4 = height * noise(t + 85);
+  const x1 = width * noise(t + 15);
+  const x2 = width * noise(t + 25);
+  const x3 = width * noise(t + 35);
+  const x4 = width * noise(t + 45);
+  const y1 = height * noise(t + 55);
+  const y2 = height * noise(t + 65);
+  const y3 = height * noise(t + 75);
+  const y4 = height * noise(t + 85);
 
   bezier(x1, y1, x2, y2, x3, y3, x4, y4);
 
   t += 0.005;
-  
+
   if (frameCount % 1000 === 0) {
     resetDraw();
   }
--- source/assets/javascripts/contact.js
+++ source/assets/javascripts/contact.js
@@ -1,13 +1,13 @@
-var a = document.createElement('a');
-a.setAttribute('href', 'mailto:ashwin@vaswani.us?Subject=Hello%20Ashwin');
-a.setAttribute('target', '_top');
+const linkEl = document.createElement('a');
+linkEl.setAttribute('href', 'mailto:ashwin@vaswani.us?Subject=Hello%20Ashwin');
+linkEl.setAttribute('target', '_top');
 
-var h1 = document.createElement('h1');
-var node = document.createTextNode('ashwin [at] vaswani.us');
+const h1 = document.createElement('h1');
+const node = document.createTextNode('ashwin [at] vaswani.us');
 h1.appendChild(node);
-a.appendChild(h1);
+linkEl.appendChild(h1);
 
-function showAddress(){
-  var check = document.getElementById('checker');
+function showAddress() {
+  let check = document.getElementById('checker');
   check.parentNode.replaceChild(a, check);
 }
