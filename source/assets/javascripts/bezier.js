498cb50b4759edc56b08cdebd3f0cb95363c2b73
ashwinv11 <ashwinv11@gmail.com>
2017-11-05 00:44:20 -0700

    first pass at turning p5 sketch to es6

--- source/assets/javascripts/bezier.js
+++ source/assets/javascripts/bezier.js
@@ -1,50 +1,55 @@
-let colorSwitch = false;
-let frameCount = 0;
-let t = 0;
+import p5 from 'p5';
 
-function setup() {
-  const myCanvas = createCanvas(380, 380);
-  myCanvas.parent('p5');
-  myCanvas.mousePressed(resetDraw);
-  myCanvas.touchStarted(resetDraw);
-  myCanvas.touchEnded(resetDraw);
-  background(242, 242, 242);
-  stroke(209, 55, 55, 25);
-  noFill();
-}
+const sketch = (p5) => {
+  let width = 380;
+  let height = 380;
+  let colorSwitch = false;
+  let frameCount = 0;
+  let t = 0;
 
-function resetDraw(){
-  colorSwitch = !colorSwitch;
-  background(242, 242, 242);
-  frameCount = 0;
-}
+  p5.setup = () => {
+    const myCanvas = p5.createCanvas(width, height);
+    myCanvas.parent('p5');
+    myCanvas.mousePressed(resetDraw);
+    myCanvas.touchStarted(resetDraw);
+    myCanvas.touchEnded(resetDraw);
+    p5.background(242, 242, 242);
+    p5.stroke(209, 55, 55, 25);
+    p5.noFill();
+  };
 
-function draw() {
+  const resetDraw = () => {
+    colorSwitch = !colorSwitch;
+    p5.background(242, 242, 242);
+    frameCount = 0;
+  };
 
-  if(!colorSwitch){
-    stroke(0, 18);
-  }
+  p5.draw = () => {
+    if(!colorSwitch) {
+      p5.stroke(0, 18);
+    } else {
+      p5.stroke(209, 55, 55, 25);
+    }
 
-  else {
-    stroke(209, 55, 55, 25);
-  }
+    const x1 = width * p5.noise(t + 15);
+    const x2 = width * p5.noise(t + 25);
+    const x3 = width * p5.noise(t + 35);
+    const x4 = width * p5.noise(t + 45);
+    const y1 = height * p5.noise(t + 55);
+    const y2 = height * p5.noise(t + 65);
+    const y3 = height * p5.noise(t + 75);
+    const y4 = height * p5.noise(t + 85);
 
-  const x1 = width * noise(t + 15);
-  const x2 = width * noise(t + 25);
-  const x3 = width * noise(t + 35);
-  const x4 = width * noise(t + 45);
-  const y1 = height * noise(t + 55);
-  const y2 = height * noise(t + 65);
-  const y3 = height * noise(t + 75);
-  const y4 = height * noise(t + 85);
+    p5.bezier(x1, y1, x2, y2, x3, y3, x4, y4);
 
-  bezier(x1, y1, x2, y2, x3, y3, x4, y4);
+    t += 0.005;
 
-  t += 0.005;
+    if(frameCount % 1000 === 0) {
+      resetDraw();
+    }
 
-  if (frameCount % 1000 === 0) {
-    resetDraw();
-  }
+    frameCount ++;
+  };
+};
 
-  frameCount += 1;
-}
+new p5(sketch);
